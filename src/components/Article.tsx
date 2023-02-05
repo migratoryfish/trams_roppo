aa41cccc1adbcf2611e5cf4f611c440a0ebd5403
migratoryfish <nuribotoke@gmail.com>
2023-02-06 01:17:30 +0900

    無限スクロールの実装

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -4,37 +4,10 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-import InfiniteScroll from "react-infinite-scroller";
-import { useState } from "react";
 import React from "react";
 import Items from "./Items";
 
-const Articles = (props: any) => {
-  // const [list, setList] = useState<any>([]); //表示するデータ
-  // const [hasMore, setHasMore] = useState(true); //再読み込み判定
-
-  // //項目を読み込むときのコールバック;
-  // const loadMore = (page: any) => {
-  //   //データ件数が0件の場合、処理終了
-  //   if (props.article.length < 1) {
-  //     setHasMore(false);
-  //     return;
-  //   }
-  //   //取得データをリストに追加
-  //   setList([...list, ...props.article.paragraphs]);
-  // };
-
-  //各スクロール要素
-  // const items = (
-  //   <ul>
-  //     {list.paragraphs.map((paragraph: any) => {
-  //       return paragraph.sentence;
-  //     })}
-  //   </ul>
-  // );
-
-  // //ロード中に表示する項目
-  // const loader = <div className="loader" key={0}></div>;
+const Article = (props: any) => {
   return (
     <Card sx={{ minWidth: 275 }}>
       {/* 読み込み最中に表示する項目 */}
@@ -62,4 +35,4 @@ const Articles = (props: any) => {
   );
 };
 
-export default Articles;
+export default Article;
