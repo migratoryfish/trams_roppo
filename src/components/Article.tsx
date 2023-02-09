cd20f299e1dd8d1201cb16623f6c9dfdc6d24c3e
migratoryfish <nuribotoke@gmail.com>
2023-02-09 10:38:10 +0900

    文内の自己法令内の条文番号の記載に順参照を実装 ツールチ の他条文番号の記載に順参照を実装 ツールチ プによる参照 先条文の表示(多段ポップアップ)

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -3,11 +3,15 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
-import React from "react";
+import React, { useState } from "react";
 import Items from "./Items";
 
 import Highlighter from "react-highlight-words";
+import { Box, ButtonBase } from "@mui/material";
+import PopUp from "./PopUp";
+import { color } from "@mui/system";
 
 const Article = (props: any) => {
   return (
@@ -23,12 +27,7 @@ const Article = (props: any) => {
             return (
               <React.Fragment key={index}>
                 {`第${index + 1}項:  `}
-                <Highlighter
-                  highlightClassName="YourHighlightClass"
-                  searchWords={[props.keyword]}
-                  autoEscape={true}
-                  textToHighlight={paragraph.sentence}
-                />
+                {getPopUpParagraph(paragraph.sentence)}
                 <Items items={paragraph.items} keyword={props.keyword} />
               </React.Fragment>
             );
@@ -38,6 +37,94 @@ const Article = (props: any) => {
       <CardActions>関連条文参照</CardActions>
     </Card>
   );
+};
+
+//TODO:これはポップアップ用文字列を作成するコンポーネントです。要修正
+//TODO:再帰だとスタックオーバーフローを起こすためループ処理に変換必須
+const getPopUpParagraph = (paragraph: string) => {
+  let { leftContext, lastMatch, rightContext } = getPopUpParser(paragraph);
+  let obj = [];
+  while (leftContext) {
+    obj.push({ leftContext, lastMatch });
+    let prevlastMatch = lastMatch;
+
+    ({ leftContext, lastMatch, rightContext } = getPopUpParser(rightContext));
+    if (prevlastMatch === lastMatch) break;
+  }
+
+  // return paragraph;
+
+  //マッチしなかった場合そのまま出力(再帰脱出口)
+  if (!leftContext) {
+    console.log("マッチしませんでした");
+    return <div>{paragraph}</div>;
+  } else {
+    // console.log("leftContext: " + leftContext);
+    // console.log("lastMatch: " + lastMatch);
+    // console.log("rightContext: " + rightContext);
+    return (
+      <div>
+        {obj.map((pg, index, obj) => {
+          return (
+            <div>
+              {pg.leftContext}
+              <Tooltip
+                title={
+                  <React.Fragment>
+                    <PopUp articleNo={pg.lastMatch} />
+                  </React.Fragment>
+                }
+                placement="bottom-start"
+                arrow
+              >
+                <div
+                  style={{ display: "inline-block", backgroundColor: "orange" }}
+                >
+                  {pg.lastMatch}
+                </div>
+              </Tooltip>
+              {index === obj.length - 1 ? rightContext : "■"}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  //マッチした場合
+  // return (
+  //   <div>
+  //     {leftContext}
+  //     <Tooltip
+  //       title={
+  //         <React.Fragment>
+  //           <PopUp />
+  //         </React.Fragment>
+  //       }
+  //       placement="bottom-start"
+  //       arrow
+  //     >
+  //       <div>{lastMatch}</div>
+  //     </Tooltip>
+  //     {getPopUpParagraph(rightContext)}
+  //   </div>
+  // );
+};
+
+const getPopUpParser = (paragraph: string) => {
+  //TODO:文字数を限定 (ex:三百二十一 で最大でも5文字なので)　一時的対応要修正
+  const matching = paragraph.match(/第.{1,5}条/);
+  // const remaining = paragraph.replace(/第.*?条/, "");
+
+  const leftContext = RegExp.leftContext;
+  const lastMatch = RegExp.lastMatch;
+  const rightContext = RegExp.rightContext;
+
+  return {
+    leftContext,
+    lastMatch,
+    rightContext,
+  };
 };
 
 export default Article;
