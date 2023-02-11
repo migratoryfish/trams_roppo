ce1b13707e19f465e30832a5f2ddc9c49369f324
migratoryfish <nuribotoke@gmail.com>
2023-02-11 18:53:14 +0900

    カッコのハイライト表示機能(3重まで対応)の実装 社会保険労務士のモックデータ投入 条文番号ジャンプのコンポーネント切り出し

--- src/components/ArticleJumpByNumber.tsx
+++ src/components/ArticleJumpByNumber.tsx
@@ -12,10 +12,19 @@ import {
   ListItemAvatar,
   ListItemButton,
   ListItemText,
+  Popper,
+  TextField,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { blue } from "@mui/material/colors";
-import { Grid3x3, Reply, ReplyAll, Scale } from "@mui/icons-material";
+import {
+  Backspace,
+  Grid3x3,
+  Reply,
+  ReplyAll,
+  Scale,
+} from "@mui/icons-material";
+import { useState } from "react";
 
 export type ArticleJumpByNumberProps = {
   open: boolean;
@@ -23,58 +32,131 @@ export type ArticleJumpByNumberProps = {
 };
 
 const ArticleJumpByNumber = (props: ArticleJumpByNumberProps) => {
-  const { onClose, open } = props;
+  //Popperテストコード
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [articleNumber, setArticleNumber] = useState<string>("");
 
-  const handleClose = (value: string) => {
-    onClose(value);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setArticleNumber("");
+    setAnchorEl(anchorEl ? null : event.currentTarget);
   };
 
-  const handleListItemClick = (value: string) => {
-    onClose(value);
+  const handleNumButton = (event: React.MouseEvent<HTMLElement>) => {
+    setArticleNumber(articleNumber + event.currentTarget.textContent);
+    console.log(
+      "ArticleJumpByNumber.handleButton: " + event.currentTarget.textContent
+    );
   };
+  const handleJump = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+    console.log("ArticleJumpByNumber.handleJump");
+  };
+
+  const handleBack = (event: React.MouseEvent<HTMLElement>) => {
+    setArticleNumber(articleNumber.slice(0, -1));
+    console.log("ArticleJumpByNumber.handleBack");
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
+
+  //Popperテストコードここまで
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle></DialogTitle>
-      <Grid>
-        <Grid>
-          <ButtonGroup>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid>
-          <ButtonGroup>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid>
-          <ButtonGroup>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid>
-          <ButtonGroup>
-            <Button>の</Button>
-            <Button>0</Button>
-            <Button>
-              <ReplyAll
-                sx={{
-                  transform: "Scale(-0.5, 0.5)",
-                  padding: "0",
-                  margin: "0",
-                }}
+    <>
+      <Box sx={{ display: "inline-block" }}>
+        <button aria-describedby={id} type="button" onClick={handleClick}>
+          条文番号ジャンプ
+        </button>
+      </Box>
+      <Popper id={id} open={open} anchorEl={anchorEl}>
+        <Box
+          sx={{
+            border: 1,
+            p: 1,
+            bgcolor: "background.paper",
+            display: "flex",
+            "& > *": {
+              m: 1,
+            },
+          }}
+        >
+          <Grid>
+            <Grid>
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="条文番号"
+                defaultValue=""
+                value={articleNumber}
               />
-            </Button>
-          </ButtonGroup>
-        </Grid>
-      </Grid>
-    </Dialog>
+            </Grid>
+            <Grid container>
+              <Grid>
+                <Grid>
+                  <ButtonGroup
+                    variant="outlined"
+                    aria-label="vertical outlined button group"
+                  >
+                    <Button key="one" onClick={handleNumButton}>
+                      1
+                    </Button>
+                    <Button key="two" onClick={handleNumButton}>
+                      2
+                    </Button>
+                    <Button key="three" onClick={handleNumButton}>
+                      3
+                    </Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid>
+                  <ButtonGroup variant="outlined">
+                    <Button onClick={handleNumButton}>4</Button>
+                    <Button onClick={handleNumButton}>5</Button>
+                    <Button onClick={handleNumButton}>6</Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid>
+                  <ButtonGroup variant="outlined">
+                    <Button onClick={handleNumButton}>7</Button>
+                    <Button onClick={handleNumButton}>8</Button>
+                    <Button onClick={handleNumButton}>9</Button>
+                  </ButtonGroup>
+                </Grid>
+                <Grid>
+                  <ButtonGroup variant="outlined">
+                    <Button onClick={handleNumButton} disabled></Button>
+                    <Button onClick={handleNumButton}>0</Button>
+                    <Button onClick={handleNumButton} disabled></Button>
+                  </ButtonGroup>
+                </Grid>
+              </Grid>
+              <Grid>
+                <Grid>
+                  <ButtonGroup orientation="vertical" variant="outlined">
+                    <Button onClick={handleNumButton}>の</Button>
+                    <Button onClick={handleBack}>
+                      <Backspace
+                        sx={{
+                          transform: "Scale(0.8, 0.8)",
+                        }}
+                      ></Backspace>
+                    </Button>
+                    <Button onClick={handleJump}>
+                      <ReplyAll
+                        sx={{
+                          transform: "Scale(-1.2, 1.2)",
+                        }}
+                      />
+                    </Button>
+                  </ButtonGroup>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+      </Popper>
+    </>
   );
 };
 
