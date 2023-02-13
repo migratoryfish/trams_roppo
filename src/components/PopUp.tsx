5f621a604bb4ae74ccae5e09e93d6bfa689f0fb3
migratoryfish <nuribotoke@gmail.com>
2023-02-13 21:10:53 +0900

    非推奨の正規表現を廃止して代替手段で実装

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,6 +1,3 @@
-import { ButtonBase, Tooltip } from "@mui/material";
-import React from "react";
-import { useState } from "react";
 import { getArticleData } from "../util/util";
 import Article from "./Article";
 
@@ -11,27 +8,6 @@ const PopUp = (props: any) => {
       <Article article={art} lawId={props.lawId} />
     </div>
   );
-
-  // return (
-  //   <div>
-  //     {"ここは前半の条文だよ"}
-  //     <Tooltip
-  //       title={
-  //         <React.Fragment>
-  //           <PopUp />
-  //         </React.Fragment>
-  //       }
-  //       placement="bottom-start"
-  //       arrow
-  //       open={isPopup}
-  //       onClose={handleMouseOut}
-  //       onOpen={handleMouseOver}
-  //     >
-  //       <div>第九十九条</div>
-  //     </Tooltip>
-  //     {"ここは後半の条文だよ"}
-  //   </div>
-  // );
 };
 
 export default PopUp;
